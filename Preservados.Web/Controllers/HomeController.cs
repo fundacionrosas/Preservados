4cd6c449afdd239892407ee19adc5242904c2a55
matiasiacono <matias@kinetica-solutions.com>
2015-06-06 13:48:53 -0300

    Initial commit

---
+++ Preservados.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Preservados.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Preservados.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetQuestions()
+        {
+            var db = new DBPreservadosDataContext();
+
+            var query = from q in db.Questions
+                            select q;
+            
+            var questions = new List<QuestionModel>();
+
+            foreach (var item in query)
+            {
+                questions.Add(new QuestionModel()
+                {
+                    Correct = item.Answer,
+                    QuestionID = item.questionId,
+                    Title = item.QuestionTitle,
+                    Questions = new List<string>()
+                    {
+                        item.Question1, item.Question2,
+                        item.Question3, item.Question4
+                    }
+                });
+            }
+            
+            return new JsonResult()
+            {
+                Data = questions,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+    }
+}
---
+++ Preservados.Web/Models/QuestionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Preservados.Web.Models
+{
+    public class QuestionModel
+    {
+        public long QuestionID { get; set; }
+
+        public string Title { get; set; }
+
+        public List<string> Questions { get; set; }
+
+        public string Correct { get; set; }
+    }
+}
